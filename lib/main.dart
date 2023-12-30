8cdc685f248d5c49143326e01563a54f868ce2bb
adityaraj1702 <aditya.raj.met21@itbhu.ac.in>
2023-12-30 17:02:58 +0530

    persistence bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test_app/on_boarding.dart';
+// import 'package:flutter_test_app/on_boarding.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,100 +29,134 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<OnBoardingEntity> _onBoardingData = OnBoardingEntity.onBoardingData;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        bottomNavigationBar: PersistentTabView(
+          context,
+          backgroundColor: Colors.blue,
+          screens: const [
+            Home(),
+            Orders(),
+            Settings(),
+            Profile(),
+            Services(),
+          ],
+          items: [
+            PersistentBottomNavBarItem(
+              icon: const Icon(Icons.home),
+              title: "Home",
+              activeColorPrimary: Colors.white,
+              activeColorSecondary: Colors.black,
+              inactiveColorPrimary: Colors.grey[400],
+            ),
+            PersistentBottomNavBarItem(
+              icon: const Icon(Icons.trolley),
+              title: "Orders",
+              activeColorPrimary: Colors.white,
+              activeColorSecondary: Colors.black,
+              inactiveColorPrimary: Colors.grey[400],
+            ),
+            PersistentBottomNavBarItem(
+              icon: const Icon(Icons.settings),
+              title: "Settings",
+              activeColorPrimary: Colors.white,
+              activeColorSecondary: Colors.black,
+              inactiveColorPrimary: Colors.grey[400],
+            ),
+            PersistentBottomNavBarItem(
+              icon: const Icon(Icons.person),
+              title: "Profile",
+              activeColorPrimary: Colors.white,
+              activeColorSecondary: Colors.black,
+              inactiveColorPrimary: Colors.grey[400],
+            ),
+            PersistentBottomNavBarItem(
+              icon: const Icon(Icons.home_repair_service),
+              title: "Services",
+              activeColorPrimary: Colors.white,
+              activeColorSecondary: Colors.black,
+              inactiveColorPrimary: Colors.grey[400],
+            ),
+          ],
+          decoration: NavBarDecoration(
+            borderRadius: BorderRadius.circular(10),
+            colorBehindNavBar: Colors.white,
+          ),
+          navBarStyle: NavBarStyle.style15,
+          screenTransitionAnimation: const ScreenTransitionAnimation(
+            animateTabTransition: true,
+            curve: Curves.easeInExpo,
+            duration: Duration(milliseconds: 200),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: PageView.builder(
-      physics: const BouncingScrollPhysics(
-          decelerationRate: ScrollDecelerationRate.fast),
-      itemCount: _onBoardingData.length,
-      itemBuilder: (context, index) {
-        return OnBoardingItem(
-            title: _onBoardingData[index].title,
-            discription: _onBoardingData[index].discription,
-            image: _onBoardingData[index].image,
-            index: index);
-      },
-    ));
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+    );
   }
 }
 
-class OnBoardingItem extends StatelessWidget {
-  final String title;
-  final String discription;
-  final String image;
-  final int index;
-
-  const OnBoardingItem(
-      {super.key,
-      required this.title,
-      required this.discription,
-      required this.image,
-      required this.index});
+class Orders extends StatelessWidget {
+  const Orders({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(
-            width: 300,
-            height: 200,
-            child: Center(
-              child: Image.asset("assets/images/$image"),
-            )),
-        const SizedBox(
-          height: 20,
-        ),
-        Text(title,
-            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-        const SizedBox(
-          height: 20,
-        ),
-        Text(
-          discription,
-          textAlign: TextAlign.center,
-          style: const TextStyle(fontSize: 18, color: Colors.grey),
-        ),
-        const SizedBox(
-          height: 50,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-                width: 10,
-                height: 10,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: index == 0 ? Colors.black : Colors.grey,
-                )),
-            const SizedBox(
-              width: 10,
-            ),
-            Container(
-                width: 10,
-                height: 10,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: index == 1 ? Colors.black : Colors.grey,
-                )),
-            const SizedBox(
-              width: 10,
-            ),
-            Container(
-              width: 10,
-              height: 10,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: index == 2 ? Colors.black : Colors.grey,
-              ),
-            ),
-          ],
-        ),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Orders"),
+      ),
+    );
+  }
+}
+
+class Settings extends StatelessWidget {
+  const Settings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+      ),
+    );
+  }
+}
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+    );
+  }
+}
+
+class Services extends StatelessWidget {
+  const Services({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Services"),
+      ),
     );
   }
 }
